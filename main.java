8aeab5101d76697eba8d555c248e3254753560e0
ENavarro <enric.navarro@insbaixcamp.cat>
2025-04-02 13:53:31 +0200

    afegir la quarta linia

--- main.java
+++ main.java
@@ -3,5 +3,6 @@ public class main {
         System.out.println("Hello");
         System.out.println("afegir una segona linia");
         System.out.println("afegir 3era linia");
+        System.out.println("afegim quarta linia");
     }
 }
