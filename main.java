65699f0a5b75686bcedd96ba88b8fa9753a88541
ENavarro <enric.navarro@insbaixcamp.cat>
2025-04-02 13:39:10 +0200

    afegir tercera linia

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("afegir una segona linia");
+        System.out.println("afegir 3era linia");
     }
 }
