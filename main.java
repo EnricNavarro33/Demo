520d902001bbb0ebecb7cf4146786367e2b9d620
ENavarro <enric.navarro@insbaixcamp.cat>
2025-04-04 14:08:34 +0200

    linia desde visual

--- main.java
+++ main.java
@@ -4,6 +4,6 @@ public class main {
         System.out.println("afegir una segona linia");
         System.out.println("afegir 3era linia");
         System.out.println("afegim quarta linia");
-        system.out.println("Hola que tal");
+        system.out.println("aquesta es un a linia afegida desde visual");
     }
 }
